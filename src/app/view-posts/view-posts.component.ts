4bac0ae3496522b778acddefe1a15df841319d7a
Charis <harmavr@gmail.com>
2023-09-23 13:39:51 +0300

    delete and update function imported

--- src/app/view-posts/view-posts.component.ts
+++ src/app/view-posts/view-posts.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PostComponent } from '../post/post.component';
 import { Post } from '../interface/post';
 import { PostService } from '../service/post.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditPostComponent } from '../edit-post/edit-post.component';
 
 @Component({
   selector: 'app-view-posts',
@@ -11,13 +12,28 @@ import { PostService } from '../service/post.service';
 export class ViewPostsComponent implements OnInit {
 
   posts: Post[]
-  constructor(private postService: PostService) {}
+  constructor(private postService: PostService, private dialog: MatDialog) {}
 
-ngOnInit(): void {
-  this.posts = this.postService.getAllposts();
-}
+  ngOnInit(): void {
+    this.GetPosts()
+  }
+
+  GetPosts() {
+    this.postService.getAllPosts().subscribe(
+      (response) => {
+        this.posts = response;
+      },
+      (error) => {
+        console.log(error)
+      },
+      () => {
+        console.log('done getting users')
+      }
+    )
+  }
+  
   isEmpty(): boolean {
-    if (this.posts.length > 0){
+    if (this.posts?.length > 0){
       return true
     }
     else {
@@ -25,8 +41,29 @@ ngOnInit(): void {
     }
   }
 
-  deletePost(post: Post ) {
-    const index = this.posts.indexOf(post)
-    this.postService.deletePost(index)
+  onDeletePost(post: Post) {
+    this.postService.deletePost(post._id)
+    this.GetPosts()
+  }
+
+  onEditPost(post: Post) {
+
+    let dialogRef = this.dialog.open(EditPostComponent, {
+      width: '1000px',
+      height: '500px',
+      data: post
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        console.log(result);
+        
+        this.postService.updatePost(post._id, result).subscribe(
+          () => {
+            this.GetPosts();
+          }
+        )
+      }
+    })
   }
 }
