f00bb4385cfd662d1ee04043062f3bbf9c32bba9
Charis <harmavr@gmail.com>
2023-09-23 13:40:46 +0300

    rework of the creation of a post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../interface/post';
 import { PostService } from '../service/post.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-posts',
@@ -12,35 +12,33 @@ export class PostsComponent implements OnInit {
 
   posts: Post[]
   showValidationsErros:boolean = false
-  fileToUpload: File | null;
-  imageUrl: string | null;
-  
+  imageUrl: string;
 
-  constructor(private postService: PostService){}
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-   
+
   }
 
-  onFileSelected(event: any) {
-    const inputElement = event.target as HTMLInputElement;
-    this.fileToUpload = inputElement.files?.[0];
-    this.imageUrl = URL.createObjectURL(this.fileToUpload);
-  }
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageUrl = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+    }
 
-    onSubmit(form: NgForm) {
-      if (form.invalid) { 
-        return this.showValidationsErros = true; 
-      }
-      console.log(form)
-      this.postService.addPost({
-        title: form.value.title,
-        content: form.value.content,
-        image: this.imageUrl
-      }
-      )
-      form.reset();
-      this.imageUrl = null;
-      return this.showValidationsErros = false;
-      }
+  onSubmit(form: NgForm) {
+    if (form.invalid) { 
+      return this.showValidationsErros = true; 
+    }
+
+    form.value.image = this.imageUrl
+    this.postService.addPost(form.value
+    )
+    form.reset();
+    this.imageUrl = null;
+    return this.showValidationsErros = false;
+    }
 }
--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient, HttpEvent, } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs'
-import { Post } from '../interface/post';
+import { Post, } from '../interface/post';
+import { Observable, map, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Response } from '../interface/response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   posts: Post[] = [];
+
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
@@ -19,19 +23,65 @@ export class PostService {
     return this.http.post('/upload', formData);
   }
 
-  getAllposts() {
-    return this.posts
+  getAllPosts(): Observable<Post[]> {
+    return this.http.get<Response>(`${this.baseUrl}/post/getAll`).pipe(
+      tap(postsArray => {
+        console.log('Posts array:')
+      }),
+      map(response => response.data.map(post => ({
+        _id: post._id,
+        title: post.title,
+        content: post.content,
+        image: post.image,
+      })))
+      
+    );
   }
 
+
   addPost(post: Post) {
-    this.posts.push(post)
+    this.http.post(`${this.baseUrl}/post/create`, post).subscribe(
+      (data: any) => {
+        console.log(data)
+      }
+       
+    )
+    
   }
-  
+
   updatePost(index: number, updatedPost: Post) {
-      this.posts[index] = updatedPost
+    return this.http.patch(`${this.baseUrl}/post/update/${index}`, updatedPost);
+    // .subscribe(
+    //   () => {
+    //     // Handle the successful deletion here, such as removing the post from the local array.
+    //     //this.posts.splice(index, 1);
+    //     this.getAllPosts()
+       
+    //   },
+      
+    //   (error) => {
+    //     console.error('Error:', error);
+    //     // Handle the error if needed
+    //   }
+    // );
+      //this.posts[index] = updatedPost
   }
 
   deletePost(index: number) {
-      this.posts.splice(index, 1)
+    console.log(index)
+    this.http.delete(`${this.baseUrl}/post/delete/${index}`).subscribe(
+      () => {
+        // Handle the successful deletion here, such as removing the post from the local array.
+        this.posts.splice(index, 1);
+        this.getAllPosts()
+       
+      },
+      
+      (error) => {
+        console.error('Error:', error);
+        // Handle the error if needed
+      }
+    );
+    
   }
 }
